4167858b95490bae4cfa3ecd3385923b685f7f65
enue <nnp@car.ocn.ne.jp>
2019-11-30 11:45:39 +0900

    add KeyAssign, MergedKeyAssign

---
+++ Runtime/KeyAssign.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public class KeyAssign : ScriptableObject
+    {
+        [System.Serializable]
+        public struct AppKey
+        {
+            public string name;
+            public KeyCode keyCode;
+        }
+
+        [System.Serializable]
+        public struct AppAxis
+        {
+            public string name;
+            public string axisName;
+        }
+
+        [System.Serializable]
+        public struct AxisButton
+        {
+            public string appKey;
+            public KeyCode keyCode;
+            public KeyCode negativeKeyCode;
+        }
+
+        [SerializeField]
+        AppKey[] appKeys = default;
+        public AppKey[] AppKeys => appKeys;
+
+        [SerializeField]
+        AppAxis[] appAxes = default;
+        public AppAxis[] AppAxes => appAxes;
+
+        [SerializeField]
+        AxisButton[] axisButtons = default;
+        public AxisButton[] AxisButtons => axisButtons;
+
+    }
+}
---
+++ Runtime/MergedKeyAssign.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace TSKT
+{
+    public readonly struct MergedKeyAssign
+    {
+        public readonly KeyAssign.AppKey[] keys;
+        public readonly string[] distinctAppKeyNames;
+        public readonly KeyAssign.AppAxis[] axes;
+        public readonly KeyAssign.AxisButton[] axisButtons;
+
+        public MergedKeyAssign(params KeyAssign[] keyAssigns)
+        {
+            {
+                var pairs = new List<KeyAssign.AppKey>();
+                foreach (var it in keyAssigns)
+                {
+                    pairs.AddRange(it.AppKeys);
+                }
+                keys = pairs.ToArray();
+                distinctAppKeyNames = pairs.Select(_ => _.name).ToArray();
+            }
+
+            {
+                var pairs = new List<KeyAssign.AppAxis>();
+                foreach (var it in keyAssigns)
+                {
+                    pairs.AddRange(it.AppAxes);
+                }
+                axes = pairs.ToArray();
+            }
+
+            {
+                var pairs = new List<KeyAssign.AxisButton>();
+                foreach (var it in keyAssigns)
+                {
+                    pairs.AddRange(it.AxisButtons);
+                }
+                axisButtons = pairs.ToArray();
+            }
+        }
+    }
+}
