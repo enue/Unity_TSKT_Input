55ca2d8a4797ceb2307f1a44dae02dd8c1d2873c
enu <otoriden@gmail.com>
2021-05-14 21:03:39 +0900

    0.5.1 KeyCodeToggle

---
+++ Assets/Package/Runtime/KeyBind/KeyCodeToggle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+namespace TSKT
+{
+    [RequireComponent(typeof(Toggle))]
+    public class KeyCodeToggle : KeyBind
+    {
+        [SerializeField]
+        KeyCode key = default;
+
+        public override bool BlockingSignals => false;
+
+        Toggle toggle;
+        Toggle Toggle => toggle ? toggle : (toggle = GetComponent<Toggle>());
+
+        public override bool OnKeyDown(List<string> keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (Toggle.isActiveAndEnabled && Toggle.interactable)
+                {
+                    Toggle.isOn = !Toggle.isOn;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool OnKeyUp(List<string> keys)
+        {
+            return false;
+        }
+
+        public override bool OnAxis(Dictionary<string, float> axisPositions)
+        {
+            return false;
+        }
+
+        public override bool OnKey(List<string> keys)
+        {
+            return false;
+        }
+
+        public override void OnSelected()
+        {
+            // nop
+        }
+    }
+}
