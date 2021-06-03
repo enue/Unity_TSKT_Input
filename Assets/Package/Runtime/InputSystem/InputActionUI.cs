9b70b6e84be1d72cb1f0a49d83203e27b7fe0939
enu <otoriden@gmail.com>
2021-06-03 10:30:25 +0900

    refactor

--- Assets/Package/Runtime/InputSystem/InputActionUI.cs
+++ Assets/Package/Runtime/InputSystem/InputActionUI.cs
@@ -8,57 +8,6 @@ namespace TSKT
 {
     public abstract class InputActionUI : MonoBehaviour
     {
-        void GenerateSortedItems(ref List<InputActionUI> result)
-        {
-            using (UnityEngine.Pool.ListPool<(RenderOrder position, InputActionUI keyBind)>.Get(out var keyBindPositions))
-            {
-                RenderOrder? maxInterceptor = default;
-
-                foreach (var item in instances)
-                {
-                    if (!item)
-                    {
-                        continue;
-                    }
-                    if (!item.gameObject.activeInHierarchy)
-                    {
-                        continue;
-                    }
-
-                    item.RefreshRootCanvas();
-                    UnityEngine.Assertions.Assert.IsTrue(item.RootCanvas);
-                    var position = new RenderOrder(item.RootCanvas!, item.transform, item.orderInObject);
-
-                    if (maxInterceptor.HasValue)
-                    {
-                        if (maxInterceptor > position)
-                        {
-                            continue;
-                        }
-                    }
-
-                    keyBindPositions.Add((position, item));
-
-                    if (item.Modal)
-                    {
-                        maxInterceptor = position;
-                    }
-                }
-                // メソッドを直接渡すとGCが発生するのでラムダにする
-                // 降順にしたいので符号を反転させる
-                keyBindPositions.Sort((x, y) => -RenderOrder.Compare(x.position, y.position));
-
-                foreach (var (position, keyBind) in keyBindPositions)
-                {
-                    result.Add(keyBind);
-                    if (keyBind.Modal)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
         static readonly HashSet<InputActionUI> instances = new HashSet<InputActionUI>();
         static bool Modified { get; set; }
 
@@ -116,10 +65,8 @@ namespace TSKT
             }
             lastUpdatedFrame = Time.frameCount;
 
-            using (UnityEngine.Pool.ListPool<InputActionUI>.Get(out var sortedItems))
+            using (BuildSortedItems(out var sortedItems))
             {
-                GenerateSortedItems(ref sortedItems);
-
                 if (Modified)
                 {
                     Modified = false;
@@ -146,6 +93,59 @@ namespace TSKT
                     }
                 }
             }
+        }
+
+        static UnityEngine.Pool.PooledObject<List<InputActionUI>> BuildSortedItems(out List<InputActionUI> result)
+        {
+            var pooledObject = UnityEngine.Pool.ListPool<InputActionUI>.Get(out result);
+            using (UnityEngine.Pool.ListPool<(RenderOrder position, InputActionUI keyBind)>.Get(out var keyBindPositions))
+            {
+                RenderOrder? maxInterceptor = default;
+
+                foreach (var item in instances)
+                {
+                    if (!item)
+                    {
+                        continue;
+                    }
+                    if (!item.gameObject.activeInHierarchy)
+                    {
+                        continue;
+                    }
+
+                    item.RefreshRootCanvas();
+                    UnityEngine.Assertions.Assert.IsTrue(item.RootCanvas);
+                    var position = new RenderOrder(item.RootCanvas!, item.transform, item.orderInObject);
+
+                    if (maxInterceptor.HasValue)
+                    {
+                        if (maxInterceptor > position)
+                        {
+                            continue;
+                        }
+                    }
+
+                    keyBindPositions.Add((position, item));
+
+                    if (item.Modal)
+                    {
+                        maxInterceptor = position;
+                    }
+                }
+                // メソッドを直接渡すとGCが発生するのでラムダにする
+                // 降順にしたいので符号を反転させる
+                keyBindPositions.Sort((x, y) => -RenderOrder.Compare(x.position, y.position));
+
+                foreach (var (position, keyBind) in keyBindPositions)
+                {
+                    result.Add(keyBind);
+                    if (keyBind.Modal)
+                    {
+                        break;
+                    }
+                }
+            }
+            return pooledObject;
         }
     }
 }
