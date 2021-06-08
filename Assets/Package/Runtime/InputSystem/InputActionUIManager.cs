ac4c1861d939c4dabe51102bd4ed022642c904fa
enu <otoriden@gmail.com>
2021-06-08 22:37:54 +0900

    0.7.3 画面遷移してもカーソル位置を覚えておくよう修正

--- Assets/Package/Runtime/InputSystem/InputActionUIManager.cs
+++ Assets/Package/Runtime/InputSystem/InputActionUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace TSKT
 {
@@ -10,6 +11,8 @@ namespace TSKT
     {
         [SerializeField]
         bool controlNavigation = false;
+
+        readonly List<GameObject?> selectedGameObjects = new List<GameObject?>();
 
         void BuildNavigation(List<InputActionUI> sortedItems)
         {
@@ -19,7 +22,8 @@ namespace TSKT
                 navigation.mode = Navigation.Mode.None;
                 it.navigation = navigation;
             }
-            GameObject? selectdGameObject = null;
+            GameObject? topSelectabeGameObject = null;
+            int? latestLog = null;
             foreach (var it in sortedItems)
             {
                 if (it.Selectable)
@@ -28,16 +32,44 @@ namespace TSKT
                     {
                         var navigation = selectable.navigation;
                         navigation.mode = Navigation.Mode.Automatic;
-                        selectdGameObject = it.gameObject;
+                        topSelectabeGameObject = it.gameObject;
                         selectable.navigation = navigation;
+
+                        var index = selectedGameObjects.IndexOf(it.gameObject);
+                        if (index > 0)
+                        {
+                            if (!latestLog.HasValue || latestLog.Value > index)
+                            {
+                                latestLog = index;
+                            }
+                        }
                     }
                 }
             }
-            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(selectdGameObject);
+            if (latestLog.HasValue)
+            {
+                EventSystem.current.SetSelectedGameObject(selectedGameObjects[latestLog.Value]);
+            }
+            else
+            {
+                EventSystem.current.SetSelectedGameObject(topSelectabeGameObject);
+            }
+
+            selectedGameObjects.RemoveAll(_ => !_);
+            selectedGameObjects.Insert(0, null);
         }
 
         void Update()
         {
+            if (controlNavigation)
+            {
+                var currentSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+                if (currentSelectedGameObject)
+                {
+                    selectedGameObjects[0] = currentSelectedGameObject;
+                }
+            }
+
             using (InputActionUI.BuildSortedItemsToActivate(out var sortedItems))
             {
                 if (InputActionUI.Modified)
